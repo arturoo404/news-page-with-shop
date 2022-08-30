214d66685189ce53b0888d785f1927d52a67c783
arturoo404 <starwars99999s@gmail.com>
2022-08-30 22:40:36 +0200

    added user registration api tests

--- src/main/java/com/arturoo404/NewsPage/entity/user/dto/UserRegistrationDto.java
+++ src/main/java/com/arturoo404/NewsPage/entity/user/dto/UserRegistrationDto.java
@@ -1,9 +1,13 @@
 package com.arturoo404.NewsPage.entity.user.dto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationDto {
     private String email;
     private String nick;
---
+++ src/test/java/com/arturoo404/NewsPage/controller/api/UserApiControllerTest.java
@@ -0,0 +1,90 @@
+package com.arturoo404.NewsPage.controller.api;
+
+import com.arturoo404.NewsPage.entity.user.dto.UserRegistrationDto;
+import com.arturoo404.NewsPage.exception.ValidException;
+import com.arturoo404.NewsPage.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@WebMvcTest(UserApiController.class)
+class UserApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @WithMockUser
+    void userRegistrationReturnStatus400WhenValidFail() throws Exception {
+        //Given
+        UserRegistrationDto userDto = UserRegistrationDto.builder()
+                .nick("account")
+                .password("password")
+                .passwordConfirm("pasword")
+                .email("email@gmail.com")
+                .build();
+
+        //When
+        when(userService.registerUser(any(UserRegistrationDto.class))).thenThrow(new ValidException("Test"));
+
+        final MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/user/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectToJson(userDto))
+                .with(csrf())).andReturn();
+        //Then
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(400);
+    }
+
+    @Test
+    @WithMockUser
+    void userRegistrationReturnStatus200AndUserEntity() throws Exception {
+        //Given
+        UserRegistrationDto userDto = UserRegistrationDto.builder()
+                .nick("account")
+                .password("password")
+                .passwordConfirm("pasword")
+                .email("email@gmail.com")
+                .build();
+
+        //When
+        doAnswer(invocationOnMock -> {
+            assertThat(invocationOnMock.getArgument(0, UserRegistrationDto.class).getEmail()).isNotBlank();
+            assertThat(invocationOnMock.getArgument(0, UserRegistrationDto.class).getNick()).isNotBlank();
+            assertThat(invocationOnMock.getArgument(0, UserRegistrationDto.class).getPassword()).isNotBlank();
+            return null;
+        }).when(userService).registerUser(any(UserRegistrationDto.class));
+
+        final MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/user/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectToJson(userDto))
+                .with(csrf())).andReturn();
+        
+        //Then
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(201);
+    }
+
+    private String objectToJson(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
