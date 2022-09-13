2e12e43d168a8192244c9449e4f5ff335a9fc2eb
arturoo404 <starwars99999s@gmail.com>
2022-09-13 14:05:38 +0200

    added geolocation api tests

--- src/main/java/com/arturoo404/NewsPage/entity/city/CityDto.java
+++ src/main/java/com/arturoo404/NewsPage/entity/city/CityDto.java
@@ -1,9 +1,11 @@
 package com.arturoo404.NewsPage.entity.city;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class CityDto {
     private String name;
     private String country;
---
+++ src/test/java/com/arturoo404/NewsPage/controller/api/GeolocationApiControllerTest.java
@@ -0,0 +1,76 @@
+package com.arturoo404.NewsPage.controller.api;
+
+import com.arturoo404.NewsPage.entity.city.CityDto;
+import com.arturoo404.NewsPage.entity.comments.dto.AddCommentsDto;
+import com.arturoo404.NewsPage.exception.NotFoundCityException;
+import com.arturoo404.NewsPage.microservice.GeolocationService;
+import com.arturoo404.NewsPage.service.ArticleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(GeolocationApiController.class)
+class GeolocationApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GeolocationService geolocationService;
+
+    private final String url = "/api/geolocation";
+
+    @Test
+    @WithMockUser
+    void itShouldThrowExceptionWhenCityWasNotFound() throws Exception {
+        //Given
+        //When
+        doThrow(new NotFoundCityException(""))
+                .when(geolocationService).getCity(anyString(), anyString());
+
+        final MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(url + "/city")
+                        .param("lat", "1")
+                        .param("lon", "0")
+                .with(csrf())).andReturn();
+
+        //Then
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(400);
+    }
+
+    @Test
+    @WithMockUser
+    void itShouldReturnCityArrayAndStatusOK() throws Exception {
+        //Given
+        CityDto [] cityDto = {new CityDto("London", "GB")};
+        //When
+        when(geolocationService.getCity(anyString(), anyString())).thenReturn(cityDto);
+
+
+        final MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(url + "/city")
+                .param("lat", "1")
+                .param("lon", "0")
+                .with(csrf()))
+                .andExpect(jsonPath("$[0].name", is("London")))
+                .andExpect(jsonPath("$[0].country", is("GB")))
+                .andReturn();
+
+        //Then
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(200);
+    }
+}
